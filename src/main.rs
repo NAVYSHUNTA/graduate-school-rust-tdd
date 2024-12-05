f8caf8305d71d49242ff65ebc220e69b2dda3d41
NAVYSHUNTA <jmsdfnavy.5491@gmail.com>
2024-12-05 11:54:12 +0900

    リファクタリング

--- src/main.rs
+++ src/main.rs
@@ -19,18 +19,30 @@ impl Dollar {
 #[cfg(test)]
 mod money_test {
     use super::*;
+
     #[test]
-    fn test_multiplication() {
+    fn test_calc1() {
+        // 50 ドル札 2 枚の価値は 100 ドル
         let fifty = Dollar { amount: 50 };
-        let result1 = fifty.times(2);
-        assert_eq!(100, result1.amount);
-        let result2 = fifty.times(3);
-        assert_eq!(150, result2.amount);
+        let res1 = fifty.times(2);
+        assert_eq!(100, res1.amount);
     }
 
     #[test]
-    fn test_equality() {
-        assert_eq!(true, Dollar { amount: 50 } == Dollar { amount: 50 });
-        assert_eq!(false, Dollar { amount: 50 } == Dollar { amount: 60 });
+    fn test_calc2() {
+        // 50 ドル札 2 枚の価値は 100 ドル
+        // 上記の副作用のテスト
+        let fifty = Dollar { amount: 50 };
+        let res1 = fifty.times(2);
+        assert_eq!(100, res1.amount);
+        let res2 = fifty.times(3);
+        assert_eq!(150, res2.amount);
+    }
+
+    #[test]
+    fn test_equals() {
+        // 50 ドル札の価値は別の 50 ドル札に等しい
+        let fifty = Dollar { amount: 50 };
+        assert_eq!(true, fifty == Dollar { amount: 50 });
     }
 }
